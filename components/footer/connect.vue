e4affb27cf3a7e67f2ed91c9297454327e852b65
nurRiyad <asadnurriyad@gmail.com>
2024-05-19 00:10:30 +0600

    Initial commit
    Signed-off-by: nurRiyad <asadnurriyad@gmail.com>

---
+++ components/footer/connect.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <p class="text-black dark:text-zinc-300   text-base font-semibold">
+      Connect With Me
+    </p>
+    <p class="dark:text-zinc-300  ">
+      I have Bachelor of social science in Mass communication and journalism. I love to write. In my free time
+      I love to write on about different topics.
+    </p>
+
+    <a
+      href="mailto: alasadnurriyad4@gmail.com"
+      class="block text-center w-full bg-sky-700 p-2 rounded-lg text-[#F1F2F4]  mt-3 text-sm"
+    >
+      Sent Mail
+    </a>
+  </div>
+</template>
---
+++ components/main/hero.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="container mx-auto">
+    <div class="grid grid-cols-1 sm:grid-cols-2 items-center">
+      <div class="px-6">
+        <h1 class="text-black dark:text-zinc-300   font-semibold leading-tight text-4xl md:text-5xl my-5">
+          Welcome To My Blog Site
+        </h1>
+        <p class="dark:text-zinc-300  ">
+          I am a open source journalism. I love to write on my free time. I am available to collaborate with other on different project
+          that can improve my learning.
+        </p>
+      </div>
+      <div class="px-6 justify-self-center">
+        <LogoDog />
+      </div>
+    </div>
+  </div>
+</template>
